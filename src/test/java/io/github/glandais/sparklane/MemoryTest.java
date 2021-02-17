9f5cd10f59a569c7fbbf7d82a9401d6ea86ac7a7
Gabriel Landais <gla@sparklane.fr>
2021-02-17 17:09:59 +0100

    Simplify

--- src/test/java/io/github/glandais/sparklane/MemoryTest.java
+++ src/test/java/io/github/glandais/sparklane/MemoryTest.java
@@ -7,10 +7,8 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockserver.client.MockServerClient;
 import org.reactivestreams.Publisher;
 import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.containers.MockServerContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
@@ -21,7 +19,6 @@ import reactor.kafka.receiver.ReceiverRecord;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderRecord;
-import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -32,33 +29,55 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.awaitility.Awaitility.await;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
 
 @Testcontainers
 public class MemoryTest {
 
     protected static final String INPUT_TOPIC = "input";
 
-    protected static final String OUTPUT_TOPIC = "output";
+    protected static final int COUNT = 100_000;
 
     @Container
     public KafkaContainer kafka = new KafkaContainer();
 
-    @Container
-    public MockServerContainer mockServer = new MockServerContainer();
-
-    private KafkaSender<String, String> sender;
-
     AtomicInteger sentInputCounter = new AtomicInteger();
-    AtomicInteger receivedInputCounter = new AtomicInteger();
-    AtomicInteger mappedCounter = new AtomicInteger();
-    AtomicInteger sentOutputCounter = new AtomicInteger();
-
-    private HttpClient httpClient;
+    AtomicInteger receivedKafkaCounter = new AtomicInteger();
+    AtomicInteger receivedProcCounter = new AtomicInteger();
 
     @BeforeEach
     public void setUp() {
+        Flux.range(0, Integer.MAX_VALUE).delayElements(Duration.ofSeconds(1)).subscribe(i -> {
+            System.out.println("------------------------------------------");
+            System.out.println("sentInputCounter     : " + sentInputCounter.get());
+            System.out.println("receivedKafkaCounter : " + receivedKafkaCounter.get());
+            System.out.println("receivedProcCounter  : " + receivedProcCounter.get());
+        });
+    }
+
+    @Test
+    public void test() {
+
+        // send COUNT messages
+        getSender().send(Flux.range(0, COUNT)
+                .map(i -> getSenderRecord(INPUT_TOPIC, i)))
+                .doOnNext(sr -> sentInputCounter.incrementAndGet())
+                .count().block();
+
+        // read messages
+        readMessages()
+                .doOnNext(rr -> receivedKafkaCounter.incrementAndGet())
+                .flatMap(this::read)
+                .subscribe(rr -> receivedProcCounter.incrementAndGet());
+
+        // await until OOM, or not
+        await().atMost(1, TimeUnit.HOURS).until(() -> receivedProcCounter.get() == COUNT);
+    }
+
+    private Publisher<ReceiverRecord<String, String>> read(ReceiverRecord<String, String> rr) {
+        return Mono.just(rr).delayElement(Duration.ofMillis(100L));
+    }
+
+    private KafkaSender<String, String> getSender() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "test-producer");
@@ -66,81 +85,7 @@ public class MemoryTest {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         SenderOptions<String, String> senderOptions = SenderOptions.create(props);
-
-        sender = KafkaSender.create(senderOptions);
-
-        new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
-                .when(request()
-                        .withPath("/test")
-                )
-                .respond(response()
-                        .withBody("Hello there !")
-                        .withDelay(TimeUnit.MILLISECONDS, 1));
-
-        Flux.range(0, Integer.MAX_VALUE).delayElements(Duration.ofSeconds(1)).subscribe(i -> {
-            System.out.println("------------------------------------------");
-            System.out.println("sentInputCounter     : " + sentInputCounter.get());
-            System.out.println("receivedInputCounter : " + receivedInputCounter.get());
-            System.out.println("mappedCounter        : " + mappedCounter.get());
-            System.out.println("sentOutputCounter    : " + sentOutputCounter.get());
-        });
-
-        httpClient = HttpClient.create();
-    }
-
-    @Test
-    public void test() {
-
-        // send messages, indefinitely
-        sendMessages(100_000);
-
-        // read messages
-        Flux<ReceiverRecord<String, String>> kafkaFlux = getMessages();
-
-        // map messages using some async
-        Flux<SenderRecord<String, String, Object>> toSend = kafkaFlux.flatMap(this::map);
-
-        // send mapped messages
-        sender.send(toSend).subscribe(sr -> sentOutputCounter.incrementAndGet());
-
-        // await until OOM, or not
-        await().atMost(1, TimeUnit.HOURS).until(() -> sentOutputCounter.get() == 90_000);
-    }
-
-    private void sendMessages(int count) {
-
-        sender.send(Flux.range(0, count)
-                .map(i -> getSenderRecord(INPUT_TOPIC, i)))
-                .doOnNext(sr -> sentInputCounter.incrementAndGet())
-                .count().block();
-    }
-
-    private Flux<ReceiverRecord<String, String>> getMessages() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "test-consumer");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        ReceiverOptions<String, String> receiverOptions = ReceiverOptions.create(props);
-
-        ReceiverOptions<String, String> options = receiverOptions.subscription(List.of(INPUT_TOPIC));
-        return KafkaReceiver.create(options).receive()
-                .doOnNext(rr -> receivedInputCounter.incrementAndGet());
-    }
-
-    private Publisher<SenderRecord<String, String, Object>> map(ReceiverRecord<String, String> rr) {
-        if (rr.offset() % 10 == 0) {
-            return Mono.empty();
-        } else {
-            return httpClient.get()
-                    .uri(mockServer.getEndpoint() + "/test")
-                    .responseContent()
-                    .aggregate()
-                    .asString()
-                    .map(s -> getSenderRecord(OUTPUT_TOPIC, mappedCounter.incrementAndGet()));
-        }
+        return KafkaSender.create(senderOptions);
     }
 
     private SenderRecord<String, String, Object> getSenderRecord(String topic, int i) {
@@ -154,4 +99,19 @@ public class MemoryTest {
         }
         return sb.toString();
     }
+
+    private Flux<ReceiverRecord<String, String>> readMessages() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "test-consumer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        ReceiverOptions<String, String> receiverOptions = ReceiverOptions.create(props);
+
+        ReceiverOptions<String, String> options = receiverOptions.subscription(List.of(INPUT_TOPIC));
+        return KafkaReceiver.create(options).receive();
+    }
+
 }
